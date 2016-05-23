151af9f68c78560a8fb7cad730209936f599dabb
sospedra <sospedra.r@gmail.com>
2016-05-23 23:11:21 +0200

    feat: add bg color and react to key press

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var body = document.getElementsByTagName('body')[0]
+  var key = document.getElementById('js-key')
+  var message = document.getElementById('js-message')
 
+  window.onkeypress = function (e) {
+    key.innerHTML = e.which
+    message.innerHTML = "key code"
+    body.style.background = 'hsl(' + e.which * 2.6 + ', 35%, 50%)'
+  }
+});
--- js/plugins.js
+++ js/plugins.js
@@ -21,4 +21,62 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+// plugins and extensions
+particlesJS(
+  "js-dots", {
+  "particles": {
+    "number": {
+      "value": 30
+    },
+    "color": {
+      "value": "#eee"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "size": {
+      "value": 15,
+      "random": true,
+      "anim": {
+        "enable": false,
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 4,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 10
+      }
+    }
+  },
+  "retina_detect": true
+});
