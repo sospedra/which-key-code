d42924d70a16731db8d784823c531b09feadca74
Rubén Sospedra <sospedra.r@gmail.com>
2016-05-24 10:45:41 +0200

    feat: add mobile support

--- js/main.js
+++ js/main.js
@@ -2,10 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
   var body = document.getElementsByTagName('body')[0]
   var key = document.getElementById('js-key')
   var message = document.getElementById('js-message')
+  var input = document.getElementById('js-input')
+
+  var updateKeyCode = function (code) {
+      key.innerHTML = code
+      message.innerHTML = "key code"
+      body.style.background = 'hsl(' + code * 2.6 + ', 35%, 50%)'
+  }
 
   window.onkeypress = function (e) {
-    key.innerHTML = e.which
-    message.innerHTML = "key code"
-    body.style.background = 'hsl(' + e.which * 2.6 + ', 35%, 50%)'
+    updateKeyCode(e.which)
   }
-});
+  input.oninput = function (e) {
+    var code = e.target.value.charCodeAt()
+
+    updateKeyCode(code)
+
+    e.target.value = ''
+  }
+
+  window.addEventListener('touchstart', function () {
+      setTimeout(function () {
+        input.focus()
+      }, 500)
+  }, false)
+})
