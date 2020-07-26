17c2418ac1c56aaad36b7188980996c3dbf4e52c
Rubén Sospedra <hello@sospedra.me>
2020-07-26 17:19:12 +0200

    feat: Add ci

--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -2,5 +2,9 @@ describe('which key code', () => {
   it('Visits the website', () => {
     cy.visit('http://localhost:1234')
     cy.get('body').type('a')
+    cy.get('#js-which').should('have.value', '65')
+    cy.get('a[href="/codes"').click()
+    cy.url().should('include', '/codes')
+    cy.get('pre').contains('Extract from')
   })
 })
